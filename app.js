03faa9860eb755fe981543274b228db324264168
jorgeperaza <jorgeperaza@hotmail.com>
2016-12-10 18:39:46 -0800

    Hiding the keys

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var builder = require('botbuilder');
 
 // Create bot and add dialogs
 var connector = new builder.ChatConnector({
-    appId: "d7c8691e-8fe3-487f-a810-3d7f5782b488",
-    appPassword: "byd1Akv2KPPNfSdZKajsPen"
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
+
 var bot = new builder.UniversalBot(connector);  
 bot.dialog('/', function (session) {
     session.send(JSON.stringify(session.message.address));
